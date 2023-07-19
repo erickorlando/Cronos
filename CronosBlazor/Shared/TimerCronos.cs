22cc83d5980b23930cc23a42f79b1fb29f132069
Erick Orlando <erickorlando@outlook.com>
2023-07-19 14:13:31 -0500

    Add project files.

---
+++ CronosBlazor/Shared/TimerCronos.cs
@@ -0,0 +1,12 @@
+﻿namespace CronosBlazor.Shared;
+
+public class TimerCronos
+{
+    public string Activity { get; set; } = default!;
+    public string DateStart { get; set; } = default!;
+    public string DateEnd { get; set; } = default!;
+
+    public int Index { get; set; }
+    public int TimeStarted { get; set; }
+    public int TimeElapsed { get; set; }
+}
